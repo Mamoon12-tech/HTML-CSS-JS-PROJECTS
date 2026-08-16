73bda88612f2119c10e662e61996d0748ef29ebf
Mamoon12-tech <hamzaandmamoon@gmail.com>
2026-08-17 02:47:03 +0500

    Add files via upload

---
+++ Project 4 Animated Portfolio Website/script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".multiple-text", {
+    strings : ["Aspiring Full-Stack Developer", "Aspiring Web Developer",
+    " Aspiring BackEnd Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+})
